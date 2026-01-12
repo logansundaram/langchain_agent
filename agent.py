4e0f5ce2653f4ada4d9b79cef99395f4bba1ffa1
Logan Sundaram <63442954+logansundaram@users.noreply.github.com>
2026-01-12 07:31:34 -0800

    experimenting with langchain agent'

--- agent.py
+++ agent.py
@@ -1,11 +1,13 @@
 # pip install -qU langchain 
 from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
+from langchain.messages import SystemMessage, HumanMessage
+from langchain.tools import tool
 
 llm = ChatOllama(model="gpt-oss:20B")
 
+@tool
 def get_weather(city: str) -> str:
-    """Get weather for a given city."""
     return f"It's always sunny in {city}!"
 
 agent = create_agent(
@@ -14,10 +16,15 @@ agent = create_agent(
     system_prompt="You are a helpful assistant",
 )
 
-# Run the agent
-answer = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
-)
+messages = []
 
+while True:
+    query = input("you: ")
+    messages.append(HumanMessage(content=query))
 
-answer
+    result = agent.invoke({"messages": messages})
+
+    messages = result["messages"]
+
+    for m in messages:
+        m.pretty_print()
