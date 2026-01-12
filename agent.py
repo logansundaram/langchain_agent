7c3b0d1b9dfa28f5e671b693b2a6c0a3ebf582bc
Logan Sundaram <63442954+logansundaram@users.noreply.github.com>
2026-01-12 06:38:54 -0800

    langchain agent

--- agent.py
+++ agent.py
@@ -1,5 +1,23 @@
+# pip install -qU langchain 
+from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 
-llm = ChatOllama(
-        model="gpt-oss:20b"
-    )
+llm = ChatOllama(model="gpt-oss:20B")
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model=llm,
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+answer = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
+
+
+answer
